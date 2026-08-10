068c82b6af0ce84d0bb3c33a80fd2cfebc51842c
Lucky Gupta <lucky100gpt@gmail.com>
2026-08-10 08:14:10 +0530

    oalindrome.cpp

---
+++ palindrome.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter a number:";
+    cin>>n;
+    int x=n;
+    int revnum=0;
+    while(n>0){
+    int ld= n%10;
+    revnum=(revnum*10)+ld;
+    n=n/10;
+    }
+    if(x==revnum){
+        cout<<"Number is palindrome";
+    }
+    else cout<<"Number is not a palindrome";
+
+
+    return 0;
+}
