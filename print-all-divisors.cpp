fa64ae1bd8ec891e8c3ad9dd90e3b63cef7b9a05
Lucky Gupta <lucky100gpt@gmail.com>
2026-08-11 05:55:04 +0530

    print-all-divisors

---
+++ print-all-divisors.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int N;
+    cout<<"Enter number:";
+    cin>>N;
+    for(int i=1;i<=N;i++){
+        // int b=N/i;
+        if(N%i==0){
+            cout<<i<<endl;
+        }
+        else{
+
+        }
+    }
+
+
+}
