b7340031a4848f9174ae356d6e86b0d99efb0a14
Lucky Gupta <lucky100gpt@gmail.com>
2026-08-14 07:33:54 +0530

    functional recursion

--- Recursion/functional_recursion.cpp
+++ Recursion/functional_recursion.cpp
@@ -1,14 +1,24 @@
+// #include<iostream>
+// using namespace std;
+
+// int func(int n) 
+// {
+// if(n==0) return 0;
+// else return n+func(n-1);
+// }
+
+
+// int main(){
+
+// cout<<func(9)<<endl;
+// }
+// factorial of N.
 #include<iostream>
 using namespace std;
-
-int func(int n) 
-{
-if(n==0) return 0;
-else return n+func(n-1);
+int factorial(int n){
+    if(n==0) return 1;
+    else return n*factorial(n-1);
 }
-
-
 int main(){
-
-cout<<func(9)<<endl;
+    cout << factorial(6)<< endl;
 }
