8a2cfa55edf678be59c361a9fa17fbc1c2c38d62
Lucky Gupta <lucky100gpt@gmail.com>
2026-09-10 18:50:37 +0530

    print linearly from 1 to n by backtrack

--- Recursion/main.cpp
+++ Recursion/main.cpp
@@ -33,15 +33,28 @@ using namespace std;
 //     return 0;
 // }
 // print numbers from N to 1.
-void func(int i, int n){
-    if(i<1)
+// void func(int i, int n){
+//     if(i<1)
+//     return;
+//     cout<< i <<" ";
+//     func(i-1,n);
+// }
+// int main(){
+//     int n;
+//     cin>> n;
+//     func(n,n);
+//     return 0;
+// }
+void f(int n,int i)
+
+{
+if(n<i)
     return;
-    cout<< i <<" ";
-    func(i-1,n);
-}
+
+cout << (n-i+1)<< endl ;
+f(n,i+1);}
 int main(){
     int n;
-    cin>> n;
-    func(n,n);
-    return 0;
+    cin >> n;
+    f(n,1);
 }
