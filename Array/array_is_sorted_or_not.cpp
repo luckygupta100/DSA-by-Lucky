f5552d96cde82ad220dbf0f3be47b3058d3339d4
Lucky Gupta <lucky100gpt@gmail.com>
2026-09-14 15:28:38 +0530

    Array is sorted or not

---
+++ Array/array_is_sorted_or_not.cpp
@@ -0,0 +1,20 @@
+// check array is sorted or not.
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int minimum = arr[0];
+    for(int i =0;i<n;i++){
+        if(arr[i]>=arr[i-1]){
+            
+        }
+        else {return false;}
+        
+    }
+    return true;
+}
